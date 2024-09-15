e480a3c3a893220269566e3acf21c8cf3b925a3b
Damian Aldair <daldairgp@gmail.com>
2024-09-15 14:42:28 -0400

    Adding docs to Material Dialog

--- lib/src/material/dialog.dart
+++ lib/src/material/dialog.dart
@@ -4,14 +4,39 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 
 import '../shared.dart';
 
+/// Changelog dialog material-based.
 class ChangelogDialog extends StatelessWidget {
+  /// Markdown file path.
+  ///
+  /// Default: "CHANGELOG.md".
   final String? changelogPath;
+
+  /// Whether the icon should be displayed.
   final bool showIcon;
+
+  /// An optional icon to display at the top of the dialog.
+  ///
+  /// Typically, an [Icon] widget. Providing an icon centers the [title]'s text.
   final Widget? icon;
+
+  /// The (optional) title of the dialog is displayed in a large font
+  /// at the top of the dialog, below the (optional) [icon].
+  ///
+  /// Typically a [Text] widget.
   final Widget? title;
+
+  /// Builder that exposes the [Markdown] to be renderer.
+  ///
+  /// If the builder is `null`, the [Markdown] will be built directly.
+  ///
+  /// Note: [Markdown] inherits from [Markdown].
   final Widget Function(BuildContext context, Widget markdown)? bodyBuilder;
+
+  /// The (optional) button that is displayed at the bottom
+  /// of the dialog with an [OverflowBar].
   final Widget? okButton;
 
+  /// Changelog dialog constructor.
   const ChangelogDialog({
     super.key,
     this.changelogPath,
