f1e7b6bdfae0a32dd91dc82190a24396fe0e14f7
Damian Aldair <daldairgp@gmail.com>
2024-09-15 14:34:46 -0400

    Adding docs to Material Screen

--- lib/src/material/screen.dart
+++ lib/src/material/screen.dart
@@ -3,15 +3,36 @@ import 'package:flutter/material.dart';
 import '../shared.dart';
 import 'raw_screen.dart';
 
+/// Changelog screen material-based.
 class ChangelogScreen extends StatelessWidget {
+  /// The primary widget displayed in the app bar.
+  ///
+  /// Typically a [Text] widget that contains a description of the current contents of the app.
   final Widget? title;
+
+  /// Controls whether we should try to imply the leading widget if null.
   final bool automaticallyImplyLeading;
+
+  /// Whether the title should be centered.
   final bool? centerTitle;
+
+  /// Markdown file path.
+  ///
+  /// Default: "CHANGELOG.md".
   final String? changelogPath;
+
+  /// Widget to be built when the file is loading.
   final Widget Function(BuildContext context)? onLoading;
+
+  /// Widget to be built when the file isload fails.
   final Widget Function(BuildContext context)? onError;
+
+  /// Builder that exposes the [Markdown] to be renderer.
+  ///
+  /// Note: [Markdown] inherits from [Markdown].
   final Widget Function(BuildContext context, Widget markdown)? bodyBuilder;
 
+  /// Changelog screen constructor.
   const ChangelogScreen({
     super.key,
     this.title,
