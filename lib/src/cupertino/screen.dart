0b6848b2d0b50f4cc0d7a148776d383468ed5c14
Damian Aldair <daldairgp@gmail.com>
2024-09-15 14:49:38 -0400

    Adding docs form Cupertino Screen

--- lib/src/cupertino/screen.dart
+++ lib/src/cupertino/screen.dart
@@ -4,15 +4,39 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 
 import '../shared.dart';
 
+/// Changelog screen cupertino-based.
 class CupertinoChangelogScreen extends StatelessWidget {
+  /// Widget to place in the middle of the navigation bar. Normally a title or
+  /// a segmented control.
+  ///
+  /// If null and [automaticallyImplyMiddle] is true, an appropriate [Text]
+  /// title will be created if the current route is a [CupertinoPageRoute] and
+  /// has a `title`.
   final Widget? middle;
+
+  /// Controls whether we should try to imply the leading widget if null.
   final bool automaticallyImplyLeading;
+
+  /// Controls whether we should try to imply the middle widget if null.
   final bool automaticallyImplyMiddle;
+
+  /// Markdown file path.
+  ///
+  /// Default: "CHANGELOG.md".
   final String? changelogPath;
+
+  /// Widget to be built when the file is loading.
   final Widget Function(BuildContext context)? onLoading;
+
+  /// Widget to be built when the file isload fails.
   final Widget Function(BuildContext context)? onError;
+
+  /// Builder that exposes the [Markdown] to be renderer.
+  ///
+  /// Note: [Markdown] inherits from [Markdown].
   final Widget Function(BuildContext context, Widget markdown)? bodyBuilder;
 
+  /// Changelog screen constructor.
   const CupertinoChangelogScreen({
     super.key,
     this.middle,
